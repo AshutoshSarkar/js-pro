a3fdfec64d1806976966b4a2a6569863d6c3990b
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2026-01-21 16:54:07 +0530

    completed strings

--- arrays.js
+++ arrays.js
@@ -1,5 +1,103 @@
-console.log("this is array file");
+// console.log("this is array file");
 
 //creating basic array;
-let numbers=[1,2,3,4,5];
-console.log(numbers);
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+
+//Insertion in array
+
+//interstion in array from the end
+
+// numbers.push(9, 10);
+// console.log(numbers);
+
+//Insertion in an array from the beginning
+// numbers.unshift(8,9);
+// console.log(numbers);
+//Insertion in an array from anywhere in the array
+// numbers.splice(4,0,'a');
+// console.log(numbers);
+
+//searching in an array
+
+// console.log(numbers.indexOf('a'));
+
+// console.log(numbers.includes('a'));
+
+//searching in an array of objects
+
+// let courses=[
+//     {userId:1, userName:'harshit'},
+//     {userId:2, userName:'mohit'},
+//     {userId:3, userName:'nitish'},
+// ];
+
+// let result=courses.find(courses=> courses.userName==='nitish');
+
+// console.log(result);
+
+//deletion in an array from the end
+// let numbers=[1,2,3,4,5];
+// let result= numbers.pop();
+// console.log(numbers);
+
+//deletion in an array from the beginning
+// let result=numbers.shift();
+// console.log(numbers);
+
+//deletion in an array from anywhere in the array
+// let result = numbers.splice(2,3);
+// console.log(numbers);
+
+
+// emotying an array
+
+// let numbers=[1,2,3,4,5];
+// let numbers2=numbers;
+
+//emptying an array method 1
+// numbers.length=0;
+
+// console.log(numbers);
+// console.log(numbers2);
+
+// console.log(numbers[0]);
+
+//emptying an array method 2
+// numbers.splice(0,numbers.length);
+
+//emptying an array method 3 Using loops
+// while(numbers.length>0){
+//     numbers2.pop();
+// }
+// console.log(numbers);
+// console.log(numbers2);
+
+
+//combining and slicing arrays
+// let first=[1,2,3];
+// let second=[4,5,6];
+
+//combining two arrays
+// const result=first.concat(second);
+// console.log(result);
+
+//slicing an array
+// const slice=result.slice(2,5);
+// console.log(slice);
+
+
+// let numbers=[10,20,30,40,50,60];
+
+ //copying an array using slice method
+// let copy=numbers.slice();
+// console.log(copy);
+
+// let numbers=[10,20,30,40,50,60];
+
+// let sliced= numbers.slice(2);
+// console.log(sliced);
+
+
+
+
